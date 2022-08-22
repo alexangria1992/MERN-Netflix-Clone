5af5e806482faac9f910e2cb3161910f355175b1
alexangria1992 <alex040892@gmail.com>
2022-08-22 17:40:38 +1200

    Featured Section

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,5 +1,6 @@
 import { AcUnit } from "@material-ui/icons";
 import React from "react";
+import Featured from "../components/featured/Featured";
 import Navbar from "../components/navbar/Navbar";
 import "./home.scss";
 
@@ -7,74 +8,8 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <img
-        width="100%"
-        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-      />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit optio ea
-      tenetur atque odit eos doloremque non quos, ipsa tempore accusamus,
-      laboriosam dolores eaque similique facilis voluptate, veritatis mollitia
-      quidem. Nam omnis, quas eos illum quidem mollitia officiis consequatur rem
-      consectetur quis voluptatem aliquid, veritatis error commodi ducimus
-      fugiat iusto, perspiciatis unde. Excepturi nesciunt illum deserunt ratione
-      autem, dolor nostrum? Deleniti beatae dolore pariatur aliquam aliquid,
-      similique, tempora ea sint eligendi ratione sed facere suscipit adipisci,
-      odio voluptate veniam? Quibusdam, odio quisquam! Eos eius fuga quasi
-      architecto vero aut sunt? Corrupti culpa dignissimos eligendi tempore sed
-      neque dolores, earum inventore ad quasi. Perspiciatis atque maiores, illum
-      quod, id mollitia quam eaque, tenetur temporibus quidem iste doloribus in
-      quas tempore! Aliquid? Nihil rerum dolorem ipsa consequuntur est tempora,
-      neque quibusdam mollitia necessitatibus possimus laudantium temporibus
-      molestiae tenetur accusamus quis eveniet, animi nesciunt enim blanditiis
-      illo! Tempora voluptas dicta hic! Dolor, vero. Enim mollitia modi nisi
-      repellendus exercitationem accusamus rerum consectetur harum laboriosam
-      dolor pariatur, dolores veniam totam est, eligendi doloremque consequuntur
-      voluptatibus sed dolore molestiae temporibus. Quos maxime deserunt animi
-      delectus? Corrupti accusantium asperiores maxime soluta? Voluptatibus
-      distinctio earum facilis, nihil atque dolores? Ea, voluptatem beatae
-      mollitia repellat unde harum rerum eos nisi animi accusantium quod ducimus
-      odit fuga error cupiditate. Adipisci aperiam sapiente a, quas repellendus
-      soluta voluptas blanditiis fugiat quisquam, placeat obcaecati harum vel
-      atque eius iusto aliquid! Dolor quo blanditiis odit ut quos fugiat dicta
-      quod, quaerat amet. In tempora dolorem nulla ullam nesciunt modi
-      distinctio harum nisi. Laudantium ducimus molestias repudiandae eos nemo
-      labore ratione optio deleniti eligendi nisi. Cupiditate sequi quidem, in
-      quod delectus rerum officiis! Nam iure, velit placeat soluta delectus
-      maiores, quos ipsum consequatur repudiandae tenetur, aspernatur dolore at
-      suscipit minus distinctio molestiae quas quod. Eum natus consequuntur vel
-      culpa, laboriosam officiis ab facilis. Veritatis similique laborum qui
-      natus, vero quasi ab odit hic saepe praesentium illo sint voluptatum
-      incidunt neque facilis labore ipsam repellat magni sapiente commodi?
-      Dolorum doloribus hic sunt explicabo iure. Voluptatum alias ut iusto
-      praesentium aliquid blanditiis tempore quaerat ab nihil, quos deserunt
-      repudiandae consequuntur numquam, explicabo rem ex qui commodi eveniet
-      fugit cum? Dignissimos numquam esse asperiores laudantium neque? Sapiente
-      nam voluptates, dolor, accusantium quam deserunt consequatur eligendi
-      labore maxime distinctio numquam veritatis recusandae laborum. Quisquam
-      sint similique in reprehenderit est, dicta perspiciatis, tempora
-      distinctio nulla repudiandae expedita qui! Reiciendis voluptates eaque
-      possimus earum dolor necessitatibus at quos, pariatur quasi explicabo
-      iusto laboriosam vel, velit optio cumque? Tempora nemo quaerat obcaecati
-      harum quam sunt sed, adipisci rem numquam quasi. Corrupti repellendus
-      suscipit at rem officiis nemo quibusdam aperiam minus voluptates
-      asperiores nostrum iure unde possimus, deserunt vero molestias doloribus
-      enim accusamus harum quasi earum! Pariatur asperiores ipsum mollitia
-      nulla. Nesciunt accusamus, facere dicta doloremque doloribus fugiat
-      ducimus alias omnis nisi quod natus illo commodi tempora exercitationem
-      rerum nemo, quos quasi quisquam asperiores, rem officiis at. Voluptates
-      est suscipit animi? Dolore dignissimos, facilis laborum nemo cupiditate
-      nulla necessitatibus. Animi quasi sit distinctio non vitae fugit nemo
-      doloribus molestiae! Sit fugit molestiae corrupti recusandae dicta
-      sapiente labore, ullam vitae facere culpa. Minima quidem deleniti sed sit
-      ipsum officia sunt quia, nemo obcaecati quos doloremque, maxime ad in ab
-      qui reiciendis optio illum repudiandae dolorem, deserunt omnis voluptatem
-      maiores placeat alias. Accusantium. Molestias qui id illum, blanditiis,
-      recusandae et maxime sequi commodi dolorem consequuntur possimus vero aut
-      accusamus perferendis dolor reiciendis voluptate repellendus, dolorum
-      voluptatum! Accusamus veritatis corporis perspiciatis, minus esse atque.
-      Error sapiente in vero tempora nam voluptas magni quos voluptates at,
-      excepturi neque quidem! Incidunt assumenda ratione, unde ipsam cupiditate
-      debitis, pariatur, dolores ullam rerum officiis cum maxime odit deserunt.
+
+      <Featured />
     </div>
   );
 };
