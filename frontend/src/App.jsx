f9f69edf500f4f0ad447094ae58919202dd911ab
alexangria1992 <alex040892@gmail.com>
2022-08-25 16:33:03 +1200

    Login Screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,9 @@ import Home from "./pages/home/Home";
 import "./app.scss";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 const App = () => {
-  return <Register />;
+  return <Home />;
 };
 
 export default App;
