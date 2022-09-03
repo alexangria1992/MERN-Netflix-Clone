c61e56611bb3095f8c9952b8ee285332bae53d9e
alexangria1992 <alex040892@gmail.com>
2022-09-03 15:26:27 +1200

    Admin Menu Component

---
+++ admin/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Topbar from "./components/topbar/Topbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar />
+      test <br />
+    </div>
+  );
+}
+
+export default App;
